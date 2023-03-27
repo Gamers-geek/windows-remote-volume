40daa280fa937e96b45f3ed833cbb7fb4fb4ba03
Gamers-geek <contact@gamers-geek.gq>
2023-03-27 21:43:30 +0200

    Fin du projet

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const audio = require("win-audio");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
@@ -16,6 +18,10 @@ server.listen(3000, () => {
 
 io.on("connection", (socket) => {
 	console.log("a user connected");
+	io.emit("audioVolume", {
+		value: audio.speaker.get(),
+		muted: audio.speaker.isMuted(),
+	});
 	socket.on("disconnect", () => {
 		console.log("user disconnected");
 	});
@@ -23,12 +29,26 @@ io.on("connection", (socket) => {
 		let soundValue = audio.speaker.get();
 		switch (data.type) {
 			case "up":
-				audio.speaker.increase(2);
+				audio.speaker.set(soundValue + 2);
 				soundValue = audio.speaker.get();
 				io.emit("audioVolume", { value: soundValue });
 				break;
 			case "down":
-				audio.speaker.decrease(2);
+				audio.speaker.set(soundValue - 2);
+				soundValue = audio.speaker.get();
+				io.emit("audioVolume", { value: soundValue });
+				break;
+			case "mute":
+				audio.speaker.mute();
+				io.emit("audioVolume", { value: 0, muted: true });
+				break;
+			case "unmute":
+				audio.speaker.unmute();
+				soundValue = audio.speaker.get();
+				io.emit("audioVolume", { value: soundValue, muted: false });
+				break;
+			case "bar":
+				audio.speaker.set(parseInt(data.value));
 				soundValue = audio.speaker.get();
 				io.emit("audioVolume", { value: soundValue });
 				break;
---
+++ public/src/js/index.js
@@ -0,0 +1,50 @@
+var socket = io();
+
+let muted = false;
+let volume = 0;
+
+const upButton = document.getElementById("volumeUpButton");
+const downButton = document.getElementById("volumeDownButton");
+const toggleMuteButton = document.getElementById("volumeToggleMute");
+const volumeBar = document.getElementById("volumeBar");
+const soundValue = document.getElementById("soundValue");
+const volumebarPercent = document.getElementById("volumeBarPercent");
+
+upButton.addEventListener("click", function () {
+	socket.emit("changeVolume", { type: "up" });
+});
+downButton.addEventListener("click", function () {
+	socket.emit("changeVolume", { type: "down" });
+});
+
+toggleMuteButton.addEventListener("click", function () {
+	socket.emit("changeVolume", { type: muted ? "unmute" : "mute" });
+});
+
+volumeBar.addEventListener("change", function (newVal) {
+	console.log(newVal.target.value);
+	socket.emit("changeVolume", { type: "bar", value: newVal.target.value });
+});
+
+socket.on("audioVolume", function (data) {
+	console.log(data);
+	muted = data.muted || false;
+	volume = data.value || 0;
+	updatePageValue();
+});
+
+function updatePageValue() {
+	if (muted == true) {
+		toggleMuteButton.children[0].children[0].classList.remove("fa-volume-high");
+		toggleMuteButton.children[0].children[0].classList.add("fa-volume-xmark");
+	} else {
+		toggleMuteButton.children[0].children[0].classList.remove(
+			"fa-volume-xmark"
+		);
+		toggleMuteButton.children[0].children[0].classList.add("fa-volume-high");
+	}
+	volumeBar.setAttribute("value", volume);
+	soundValue.innerText = `${volume}%`;
+	volumebarPercent.value = `${volume}%`;
+}
+bulmaSlider.attach();
